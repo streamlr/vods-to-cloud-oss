cf10a2bd303e4ef60850185e63d7ca2cab0ddc64
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-08-19 20:22:01 -0600

    Refactor app.py: implement token update functionality and schedule token refresh every hour; update requirements.txt to include schedule package

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
-from src.utils import get_saved_twitch_tokens
+from src.utils import get_saved_twitch_tokens, get_access_token_from_refresh
 from auth_server import auth_server
+import json
 import os
 import time
 import schedule
@@ -11,11 +12,28 @@ CLIENT_ID = os.getenv("TWITCH_CLIENT_ID")
 CLIENT_SECRET = os.getenv("TWITCH_CLIENT_SECRET")
 REDIRECT_URI = os.getenv("TWITCH_REDIRECT_URI")
 
+tokens = None
+
+
+def update_token():
+    global tokens
+
+    REFRESH_TOKEN = tokens.get("refresh_token")
+
+    tokens = get_access_token_from_refresh(CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN)
+
 
 def main():
+    global tokens
+
     tokens = auth_server(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
 
-    print("Tokens received:", tokens)
+    schedule.every(1).hours.do(update_token)
+
+    while True:
+        schedule.run_pending()
+
+        time.sleep(1)
 
 
 if __name__ == "__main__":
