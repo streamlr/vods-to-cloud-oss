9a4744d8aa2713ba208fb6b3354b523c91f92104
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-08-24 16:31:40 -0600

    Refactor get_all_vods function in vods.py to include USER_ID parameter for retrieving VODs

--- src/utils/vods.py
+++ src/utils/vods.py
@@ -1,14 +1,18 @@
 import requests
 
 
-def get_all_vods(CLIENT_ID: str, ACCESS_TOKEN: str):
+def get_all_vods(CLIENT_ID: str, ACCESS_TOKEN: str, USER_ID: str):
     headers = {
         "Client-ID": CLIENT_ID,
         "Authorization": f"Bearer {ACCESS_TOKEN}",
         "Content-Type": "application/json"
     }
 
-    response = requests.get("https://api.twitch.tv/helix/videos", headers=headers)
+    params = {
+        "user_id": USER_ID
+    }
+
+    response = requests.get("https://api.twitch.tv/helix/videos", headers=headers, params=params)
 
     if response.status_code == 200:
         return response.json()
@@ -17,14 +21,23 @@ def get_all_vods(CLIENT_ID: str, ACCESS_TOKEN: str):
         return None
 
 
-def get_new_vod(CLIENT_ID: str, ACCESS_TOKEN: str):
-    all_vods = get_all_vods(CLIENT_ID, ACCESS_TOKEN)
+def get_latest_vod(CLIENT_ID: str, ACCESS_TOKEN: str, USER_ID: str):
+    all_vods = get_all_vods(CLIENT_ID, ACCESS_TOKEN, USER_ID)
 
 
 if __name__ == "__main__":
-    from sys import argv
+    from dotenv import load_dotenv
+    from os import getenv
+    from twitch_oauth import get_saved_twitch_tokens
+    from user_data import get_user_data
 
-    CLIENT_ID = argv[1]
-    ACCESS_TOKEN = argv[2]
+    load_dotenv()
 
-    print(get_all_vods(CLIENT_ID, ACCESS_TOKEN))
+    CLIENT_ID = getenv("TWITCH_CLIENT_ID")
+    ACCESS_TOKEN = get_saved_twitch_tokens()["access_token"]
+    USER_ID = get_user_data(CLIENT_ID, ACCESS_TOKEN)["id"]
+
+    print("VOD data:", CLIENT_ID, ACCESS_TOKEN, USER_ID, sep="\n")
+
+    vod_data = get_all_vods(CLIENT_ID, ACCESS_TOKEN, USER_ID)
+    print("VOD data:", vod_data)
