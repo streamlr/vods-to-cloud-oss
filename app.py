cfaa545c004322a5c4c8aabcd44b2bdaea71fe0c
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-08-24 00:07:04 -0600

    Refactor authentication flow: remove auth_server.py, integrate server logic into app.py, and streamline token handling

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from dotenv import load_dotenv
-from src.utils import get_saved_twitch_tokens, get_access_token_from_refresh
-from auth_server import auth_server
-import json
+from src.utils import server
 import os
-import time
-import schedule
 
 load_dotenv()
 
@@ -12,30 +8,9 @@ CLIENT_ID = os.getenv("TWITCH_CLIENT_ID")
 CLIENT_SECRET = os.getenv("TWITCH_CLIENT_SECRET")
 REDIRECT_URI = os.getenv("TWITCH_REDIRECT_URI")
 
-tokens = None
-
-
-def update_token():
-    global tokens
-
-    REFRESH_TOKEN = tokens.get("refresh_token")
-
-    tokens = get_access_token_from_refresh(CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN)
-
 
 def main():
-    global tokens
-
-    tokens = auth_server()
-
-    schedule.every(1).hours.do(update_token)
-
-    while True:
-        schedule.run_pending()
-
-        print(tokens)
-
-        time.sleep(1)
+    server(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
 
 
 if __name__ == "__main__":
---
+++ src/utils/server.py
@@ -0,0 +1,46 @@
+from dotenv import load_dotenv
+from flask import Flask, request
+from src.utils import get_auth_url, get_twitch_tokens_from_server
+import os
+
+
+def server(CLIENT_ID: str, CLIENT_SECRET: str, REDIRECT_URI: str):
+    app = Flask(__name__)
+
+
+    @app.route("/")
+    def index():
+        auth_url = get_auth_url(CLIENT_ID, REDIRECT_URI)
+
+        login_page_path = os.path.join("src", "pages", "login.html")
+        with open(login_page_path, "r") as f:
+            return f.read().replace("{{auth_url}}", auth_url)
+
+        return f"<a href=\"{auth_url}\">Login with Twitch</a>"
+
+
+    @app.route("/callback")
+    def callback():
+        code = request.args.get("code")
+        if not code:
+            return "Error: no code received", 400
+
+        get_twitch_tokens_from_server(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, code)
+
+        callback_page_path = os.path.join("src", "pages", "callback.html")
+        with open(callback_page_path, "r") as f:
+            return f.read()
+
+        return "Login successful, you can close this tab now."
+
+
+    @app.route("/webhook", methods=["POST"])
+    def webhook():
+        data = request.json
+
+        print("Received webhook:", data)
+
+        return "Webhook received", 200
+
+
+    app.run(port=5000, debug=False, use_reloader=False)
