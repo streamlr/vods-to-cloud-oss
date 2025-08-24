64f8453f64df1c946aab51632971ca34f7a19762
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-08-24 16:05:30 -0600

    Refactor token handling in twitch_oauth.py: streamline token path definition and improve JSON loading

--- src/utils/twitch_oauth.py
+++ src/utils/twitch_oauth.py
@@ -1,7 +1,9 @@
-from json import load as json_load
 from os import path
+import json
 import requests
 import urllib
+
+tokens_path = path.join("src", "data", "tokens.json")
 
 
 def get_auth_url(CLIENT_ID: str, REDIRECT_URI: str) -> str:
@@ -27,18 +29,20 @@ def get_twitch_tokens_from_server(CLIENT_ID: str, CLIENT_SECRET: str, REDIRECT_U
     }
     resp = requests.post(token_url, data=data)
     tokens = resp.json()
+
+    with open(tokens_path, "w") as f:
+        json.dump(tokens, f)
+
     return tokens
 
 
 def get_saved_twitch_tokens() -> dict | None:
-    tokens_path = path.join("src", "data", "tokens.json")
-
     if not path.exists(tokens_path):
         print("Tokens file does not exist. Please run the OAuth flow first.")
         return None
 
     with open(tokens_path, "r") as f:
-        tokens = json_load(f)
+        tokens = json.load(f)
 
     if "access_token" not in tokens:
         print("Access token is missing. Please run the OAuth flow first.")
