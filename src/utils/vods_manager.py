085b24ce23836c37af51e2dc65a0a9f98de48888
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-08-25 00:26:29 -0600

    Rename and update VOD deletion function to use DELETE method with correct parameters

--- src/utils/vods_manager.py
+++ src/utils/vods_manager.py
@@ -1,24 +1,24 @@
 import requests
 
 
-def hide_vod_by_id(CLIENT_ID: str, ACCESS_TOKEN: str, VOD_ID: str):
+def delete_vod_by_id(CLIENT_ID: str, ACCESS_TOKEN: str, VOD_ID: str):
     url = f"https://api.twitch.tv/helix/videos"
     headers = {
         "Client-ID": CLIENT_ID,
-        "Authorization": f"Bearer {ACCESS_TOKEN}"
+        "Authorization": f"Bearer {ACCESS_TOKEN}",
+        "Content-Type": "application/json"
     }
-    data = {
-        "id": VOD_ID,
-        "viewable": "private"
+    params = {
+        "id": VOD_ID
     }
 
-    resp = requests.patch(url, headers=headers, json=data)
+    response = requests.delete(url, headers=headers, params=params)
 
-    return resp.json()
+    return response.json()
 
 
 if __name__ == "__main__":
     from test_utils import test_util
 
-    response = test_util(hide_vod_by_id, ["TWITCH_CLIENT_ID", "ACCESS_TOKEN", "2548950092"])
+    response = test_util(delete_vod_by_id, ["TWITCH_CLIENT_ID", "ACCESS_TOKEN", "2548963347"])
     print("Response:", response)
