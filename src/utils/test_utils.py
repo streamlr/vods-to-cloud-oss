f60892d61dc04b9b0f6f0f8838c5280a36d6b968
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-08-27 10:50:29 -0600

    Add USER_ID retrieval to test utility function

--- src/utils/test_utils.py
+++ src/utils/test_utils.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from twitch_oauth import get_saved_twitch_tokens
+from user_data import get_user_id
 from os import getenv
 
 load_dotenv()
@@ -13,6 +14,10 @@ def test_util(callback: callable, args: list, debug: bool = True, showEnv: bool 
     for arg in args:
         if arg == "ACCESS_TOKEN":
             VAR_ENVIRONMENTS.append(get_saved_twitch_tokens().get("access_token"))
+        elif arg == "USER_ID":
+            CLIENT_ID = getenv("TWITCH_CLIENT_ID")
+            ACCESS_TOKEN = get_saved_twitch_tokens().get("access_token")
+            VAR_ENVIRONMENTS.append(get_user_id(CLIENT_ID, ACCESS_TOKEN))
         elif getenv(arg):
             VAR_ENVIRONMENTS.append(getenv(arg))
         else:
